a358040e1b48434b6541c7373096e747f24776d5
fmcurti <fcurti@DESKTOP-RRQURBM.localdomain>
2020-06-19 21:43:16 -0300

    BugFix indices del grafo

--- grafo.cpp
+++ grafo.cpp
@@ -9,9 +9,9 @@ Grafo leerGrafo(bool dirigido) {
     for (int i = 0; i < m; i++) {
         int v, w, peso;
         cin >> v >> w >> peso;
-        G[v].push_back(Vecino(w - 1, peso));
+        G[v - 1].push_back(Vecino(w - 1, peso));
         if (!dirigido) {
-            G[w].push_back(Vecino(v-1, peso));
+            G[w - 1].push_back(Vecino(v-1, peso));
         }
     }
     return G;
