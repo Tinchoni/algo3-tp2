aa7cbfa0c4b2f7d0c0c918b6b9f54af3fcf5ccc4
Uriel Chami <uriel@botbit.io>
2020-11-02 08:54:36 -0300

    Algunos pdfs que quedaron colgados

--- experimentos/exp5_tabu_complejidad_memo.py
+++ experimentos/exp5_tabu_complejidad_memo.py
@@ -6,30 +6,30 @@ Created on Thu Jul  2 18:52:43 2020
 """
 from general import *
 
-inp_ari = "--algoritmo Tabu --solInicial AGM --itParada 50 --tipoMemoria aristas --tamanioMemoria %i"
+inp_ari = "--algoritmo Tabu --solInicial AGM --itParada 50 --tipoMemoria aristas --tamanioMemoria %i -d 30"
 out_ari = dict()
-inp_sol = "--algoritmo Tabu --solInicial AGM --itParada 50 --tipoMemoria soluciones --tamanioMemoria %i"
+inp_sol = "--algoritmo Tabu --solInicial AGM --itParada 50 --tipoMemoria soluciones --tamanioMemoria %i -d 30"
 out_sol = dict()
 
-grafos = ["gr21", "gr48", "gr96", "gr202"]#, "gr431"]
+grafos = ["gr48", "gr202"]#,"gr21","gr96","gr431"]
+memorias = list(range(1, 71, 10))
 
-for nombre in grafos:
-	out_ari[nombre] = []
-	out_sol[nombre] = []
-	gra = cargarGrafo_tsp("optimos/%s.tsp"%nombre)
+# for nombre in grafos:
+# 	out_ari[nombre] = []
+# 	out_sol[nombre] = []
+# 	gra = cargarGrafo_tsp("optimos/%s.tsp"%nombre)
 	
-	memorias = list(range(1, 71, 1))
-	for M in memorias:
-		out_ari[nombre].append( tprun(inp_ari%M, gra) )
-		if not out_ari[nombre][-1].verificar(gra):
-			raise Exception("Ups!")
+# 	for M in memorias:
+# 		out_ari[nombre].append( tprun(inp_ari%M, gra) )
+# 		if not out_ari[nombre][-1].verificar(gra):
+# 			raise Exception("Ups!")
 		
-		out_sol[nombre].append( tprun(inp_sol%M, gra) )
-		if not out_sol[nombre][-1].verificar(gra):
-			raise Exception("Ups!")
+# 		out_sol[nombre].append( tprun(inp_sol%M, gra) )
+# 		if not out_sol[nombre][-1].verificar(gra):
+# 			raise Exception("Ups!")
 
-with open("exp5.pkl", 'wb') as f:
-	pickle.dump((grafos, out_ari, out_sol), f)
+# with open("exp5.pkl", 'wb') as f:
+# 	pickle.dump((grafos, out_ari, out_sol), f)
 
 #%%
 with open("exp5.pkl", 'rb') as f:
@@ -38,6 +38,8 @@ with open("exp5.pkl", 'rb') as f:
 #%%
 c=0
 plt.figure(1)
+plt.figure(figsize=(9,6))
+
 for nombre in grafos:
 	ts = np.array( [x.tiempo for x in out_ari[nombre]] )/1000
 	plt.plot(memorias, ts, '-C%i'%c, label=nombre)
@@ -58,17 +60,18 @@ plt.savefig('tabu_complejidad_memoria.pdf')
 
 c=0
 plt.figure(2)
+plt.figure(figsize=(9,6))
 
 for nombre in ["gr48", "gr202"]:
 	cs = np.array( [x.costo for x in out_ari[nombre]] )/costos_optimos[nombre]
-	plt.plot(memorias[:60], cs[:60], '-C%i'%c, label=nombre)
+	plt.plot(memorias[:], cs[:], '-C%i'%c, label=nombre)
 	
 	cs = np.array( [x.costo for x in out_sol[nombre]] )/costos_optimos[nombre]
-	plt.plot(memorias[:60], cs[:60], '--C%i'%c)
+	plt.plot(memorias[:], cs[:], '--C%i'%c)
 	c += 1
 	
 plt.xlabel('Memoria tabú')
 plt.ylabel('Costo del ciclo hallado (relativo al óptimo)')
-plt.xlim([memorias[0], memorias[60]])
+plt.xlim([memorias[0], memorias[-1]])
 plt.legend(title="Grafo")
 plt.savefig('tabu_costo_memoria.pdf')
